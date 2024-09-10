1fd9c52636743ecfe0032e4da52bb281af88e4f8
Reemaalhawas <93104215+Reemaalhawas@users.noreply.github.com>
2024-09-11 02:35:22 +0300

    GET request made to imdb server

--- Scraping.py
+++ Scraping.py
@@ -1,20 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
-# The URL for the IMDB Top 250 Movies
 url = 'https://www.imdb.com/chart/top/'
 
-# Define headers to mimic a web browser request
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 
-# Send the HTTP GET request with headers
 response = requests.get(url, headers=headers)
 
-# Check if the request was successful
 if response.status_code == 200:
-    print("Page successfully retrieved!")
-    # You can print the content or parse the HTML here
-    print(response.content)  # This will print the raw HTML of the page
+    print("Page retrieved")
+  
+    print(response.content) 
 else:
     print(f"Failed to retrieve page. Status code: {response.status_code}")
+
